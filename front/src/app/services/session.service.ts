0e96ca1f56cfee3d29e70b5a0f0234b2b551636a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-22 20:24:31 +0100

    session service ut : login

--- front/src/app/services/session.service.ts
+++ front/src/app/services/session.service.ts
@@ -10,6 +10,7 @@ export class SessionService {
   public isLogged = false;
   public sessionInformation: SessionInformation | undefined;
 
+  // emits islogged on every change
   private isLoggedSubject = new BehaviorSubject<boolean>(this.isLogged);
 
   public $isLogged(): Observable<boolean> {
@@ -19,16 +20,16 @@ export class SessionService {
   public logIn(user: SessionInformation): void {
     this.sessionInformation = user;
     this.isLogged = true;
-    this.next();
+    this.next(); // ask for an emittion 
   }
 
   public logOut(): void {
     this.sessionInformation = undefined;
     this.isLogged = false;
-    this.next();
+    this.next(); // ask for an emittion 
   }
 
   private next(): void {
-    this.isLoggedSubject.next(this.isLogged);
+    this.isLoggedSubject.next(this.isLogged); // start an emission
   }
 }
