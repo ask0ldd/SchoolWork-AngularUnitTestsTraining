866c518fac0b6ea67947133eafa967aa79f79009
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-22 06:05:11 +0100

    teacher service ut : all

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,11 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import { of } from 'rxjs';
+
+const teacher = {
+  id: 1,
+  lastName: "lastname",
+  firstName: "firstname",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpClient : HttpClient
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,9 +25,18 @@ describe('TeacherService', () => {
       ]
     });
     service = TestBed.inject(TeacherService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('all', () => {
+    const httpClientSpy = jest.spyOn(httpClient, 'get').mockReturnValue(of([{...teacher}, {...teacher}]))
+    service.all().subscribe(teachers => {
+      expect(teachers).toEqual([{...teacher}, {...teacher}])
+      expect(httpClientSpy).toHaveBeenCalledWith("api/teacher")
+    })
+  })
 });
