60d8bdee167614db38fbbd8bcfb88896963e1b9d
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-22 17:53:10 +0100

    user service ut : getuserbyidl

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,11 +1,35 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import { User } from '../interfaces/user.interface';
+import { of } from 'rxjs';
+
+const user : User = {
+  id: 1,
+  email: 'email@email.com',
+  firstName: 'string',
+  lastName: 'string',
+  admin: true,
+  password: 'password',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+
+/*
+  id: number;
+  email: string;
+  lastName: string;
+  firstName: string;
+  admin: boolean;
+  password: string;
+  createdAt: Date;
+  updatedAt?: Date;*/
 
 describe('UserService', () => {
   let service: UserService;
+  let httpClient : HttpClient
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,9 +38,18 @@ describe('UserService', () => {
       ]
     });
     service = TestBed.inject(UserService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getById', () => {
+    const httpClientSpy = jest.spyOn(httpClient, 'get').mockReturnValue(of({...user}))
+    service.getById("1").subscribe(retUser => {
+      expect(retUser).toEqual({...user})
+      expect(httpClientSpy).toHaveBeenCalledWith("api/user/1")
+    })
+  })
 });
