4d3c5f5ab0f12ab40bac577256119e0f3423f24b
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-26 18:49:24 +0100

    context ut ok

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,13 +1,16 @@
 package com.openclassrooms.starterjwt;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads(ApplicationContext context) {
+		Assertions.assertThat(context).isNotNull();
 	}
 
 }
