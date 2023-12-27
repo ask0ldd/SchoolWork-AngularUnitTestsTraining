4f8ae3ebc87706a8bc5cf328b4ba2d2b6b5ddfa0
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-27 19:57:10 +0100

    all repo ut base done

---
+++ back/src/test/java/com/openclassrooms/starterjwt/repository/SessionRepositoryTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.starterjwt.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@SpringBootTest(classes = { com.openclassrooms.starterjwt.SpringBootSecurityJwtApplication.class })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class SessionRepositoryTests {
+
+}
---
+++ back/src/test/java/com/openclassrooms/starterjwt/repository/UserRepositoryTests.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.starterjwt.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+@SpringBootTest(classes = { com.openclassrooms.starterjwt.SpringBootSecurityJwtApplication.class })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+
+}
