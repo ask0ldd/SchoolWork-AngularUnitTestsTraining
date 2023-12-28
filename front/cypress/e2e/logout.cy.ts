378d4ac129a2a6b5ad306f4e257ca023beff1995
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-29 00:52:27 +0100

    cypress logout

---
+++ front/cypress/e2e/logout.cy.ts
@@ -0,0 +1,34 @@
+describe('Logout spec', () => {
+    it('Logout successfull', () => {
+      cy.visit('/login')
+  
+      cy.intercept('POST', '/api/auth/login', {
+        body: {
+          id: 1,
+          username: 'userName',
+          firstName: 'firstName',
+          lastName: 'lastName',
+          admin: true
+        },
+      })
+  
+      cy.intercept(
+        {
+          method: 'GET',
+          url: '/api/session',
+        },
+        []).as('session')
+  
+      cy.get('input[formControlName=email]').type("yoga@studio.com")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+  
+      cy.url().should('include', '/sessions')
+
+      cy.contains('span', 'Logout').click()
+
+      cy.contains('span', 'Login').should('exist')
+
+      cy.url().should('equal', 'http://localhost:4200/')
+
+    })
+  });
