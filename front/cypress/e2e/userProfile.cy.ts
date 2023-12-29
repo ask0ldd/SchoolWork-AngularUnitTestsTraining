a113bec8f850e910b978fcc5debafec33bd93e26
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-29 02:46:44 +0100

    root cypress user profile fixture

--- front/cypress/e2e/userProfile.cy.ts
+++ front/cypress/e2e/userProfile.cy.ts
@@ -1,34 +1,46 @@
 describe('User Profile spec', () => {
     it('Access User Profile', () => {
-      cy.visit('/login')
-  
-      cy.intercept('POST', '/api/auth/login', {
+        cy.visit('/login')
+
+        cy.intercept('POST', '/api/auth/login', {
         body: {
-          id: 1,
-          username: 'userName',
-          firstName: 'firstName',
-          lastName: 'lastName',
-          admin: true
+            id: 1,
+            username: 'userName',
+            firstName: 'firstName',
+            lastName: 'lastName',
+            admin: true
         },
-      })
-  
-      cy.intercept(
+        })
+
+        cy.intercept(
         {
-          method: 'GET',
-          url: '/api/session',
+            method: 'GET',
+            url: '/api/session',
         },
         []).as('session')
-  
-      cy.get('input[formControlName=email]').type("yoga@studio.com")
-      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-  
-      cy.url().should('include', '/sessions')
 
-      cy.contains('span', 'Account').click()
+        cy.get('input[formControlName=email]').type("yoga@studio.com")
+        cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
-      cy.contains('p', 'Name: Admin ADMIN').should('exist')
-      cy.contains('p', 'Email: yoga@studio.com').should('exist')
-      cy.contains('p', 'You are admin').should('exist')
-      cy.contains('p', 'December 29, 2023').should('exist')
+        cy.url().should('include', '/sessions')
+
+        /*cy.intercept(
+        {
+            method: 'GET',
+            url: '/api/user/1',
+        },
+        []).as('user')*/
+        cy.intercept('GET', '/api/user/*', { fixture: 'user.json' })
+
+        cy.contains('span', 'Account').click()
+
+        // cy.wait('@user').then((interception) => {
+        
+
+        cy.contains('p', 'Name: Admin ADMIN').should('exist')
+        cy.contains('p', 'Email: yoga@studio.com').should('exist')
+        cy.contains('p', 'You are admin').should('exist')
+        cy.contains('p', 'December 29, 2023').should('exist')
+        // })
     })
   });
