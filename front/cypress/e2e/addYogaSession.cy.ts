cb98240b051417259b19d8a7df3346b8d4dbafa3
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-12-29 17:33:49 +0100

    yoga session displayed after form submission

--- front/cypress/e2e/addYogaSession.cy.ts
+++ front/cypress/e2e/addYogaSession.cy.ts
@@ -50,7 +50,12 @@ describe('Login spec', () => {
 
       cy.get('button[type=submit]').should('exist').click()
 
+      cy.contains('mat-card-title', 'Rentals available').should('exist')
+      cy.contains('mat-card-title', 'yoga fire').should('exist')
+      cy.contains('p', 'yoga fire description').should('exist')
+      cy.contains('mat-card-subtitle', 'December 29, 2024').should('exist')
 
+      cy.get('img[src="assets/sessions.png"]').should('exist')
 
       })
   });
